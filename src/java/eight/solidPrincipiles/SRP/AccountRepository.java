2dec9af8caa61024f3796c2784b85164a35b0a75
Rohit Shelar <rohit.shelar@riomed.com>
2022-01-11 12:44:51 +0530

    solid principle - SRP added

---
+++ src/java/eight/solidPrincipiles/SRP/AccountRepository.java
@@ -0,0 +1,8 @@
+package java.eight.solidPrincipiles.SRP;
+
+public class AccountRepository{
+	public void create(){
+		//2 
+		System.out.println("Save to database");
+	}
+}
---
+++ src/java/eight/solidPrincipiles/SRP/NotificationService.java
@@ -0,0 +1,8 @@
+package java.eight.solidPrincipiles.SRP;
+
+public class NotificationService{
+	public void sendNotification(){
+		//3 
+		System.out.println("Send notification");
+	}
+}
