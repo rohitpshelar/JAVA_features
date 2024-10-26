160055f5dce492a890c8919289a9076f3aed0d59
Rohit Shelar <rohitpshelar@gmail.com>
2024-10-26 11:42:42 +0530

    package changed

---
+++ src/com/SolidPrincipiles/A_SolidRepositoryPrinciple.java
@@ -0,0 +1,47 @@
+package com.SolidPrincipiles;
+
+// Solid Repository Principle
+class Account {
+
+	 void OpenAccount() {
+		// 1
+		System.out.println("Fill Account details");
+
+		// 2
+		System.out.println("Save to database");
+
+		// 3
+		System.out.println("Send notification");
+	}
+}
+
+//===================================================^=OLD ==========
+class AccountRepository {
+	public void create() {
+		// 2
+		System.out.println("Save to database");
+	}
+}
+
+class NotificationService {
+	public void sendNotification() {
+		// 3
+		System.out.println("Send notification");
+	}
+}
+
+//A class should have one and only one reason to change - 
+public class A_SolidRepositoryPrinciple{
+	AccountRepository accountRepository = new AccountRepository();
+	NotificationService notificationService = new NotificationService();
+
+	 void openAccount() {
+		// 1
+		System.out.println("Fill Account details");
+		//2
+		accountRepository.create();
+		//3
+		notificationService.sendNotification();
+
+	}
+}
---
+++ src/com/VolatileKeyword/VolatileKeyword.java
@@ -0,0 +1,41 @@
+package com.VolatileKeyword;
+
+// Thread
+// local cache - default
+// Sheared cache - activate using volatile keyword
+public class VolatileKeyword {
+
+	public boolean vola = true;
+
+	public boolean isVola() {
+		return vola;
+	}
+
+	public void setVola(boolean vola) {
+		this.vola = vola;
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		VolatileKeyword volatileKeyword = new VolatileKeyword();
+
+		new Thread() {
+			public void run() {
+				int count = 0;
+				while (volatileKeyword.vola) {
+					count = count +1;
+					System.out.println(count + " Running..");
+				}
+			};
+		}.start();
+		
+		Thread.sleep(1);
+	
+		new Thread() {
+			public void run() {
+				volatileKeyword.vola = false;
+			};
+		}.start();
+
+	}
+
+}
